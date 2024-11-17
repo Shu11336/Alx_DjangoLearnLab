192edb41549041114ae8d04feedba0842759db8b
Shu11336 <moazshouman@gmail.com>
2024-11-17 23:04:20 +0200

    z

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,46 +1,36 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
-class UserProfile(models.Model):
-    ROLE_CHOICES = [
-        ('Admin', 'Admin'),
-        ('Librarian', 'Librarian'),
-        ('Member', 'Member'),
-    ]
-    
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='Member')
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, null=True, blank=True)  # Added title field
 
     def __str__(self):
-        return f"{self.user.username} - {self.role}"
-
-
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.userprofile.save()
-
+        return self.name
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
-    publication_year = models.IntegerField()
-
-    # Adding the custom permissions
-    class Meta:
-        permissions = [
-            ("can_add_book", "Can add a book"),
-            ("can_change_book", "Can change a book"),
-            ("can_delete_book", "Can delete a book"),
-        ]
+    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, null=True, blank=True)  # Added title field
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
 
     def __str__(self):
-        return self.title
+        return self.name
+
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, null=True, blank=True)  # Added title field
+    books = models.ManyToManyField(Book, related_name='libraries')
+
+    def __str__(self):
+        return self.name
+
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, null=True, blank=True)  # Added title field
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
