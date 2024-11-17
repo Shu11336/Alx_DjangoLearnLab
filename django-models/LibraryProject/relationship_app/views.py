07cdbee0d652f4a26ca2eaf016288d289ba7eb88
Shu11336 <moazshouman@gmail.com>
2024-11-17 23:21:23 +0200

    aa

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,9 @@
 # views.py
 
-from django.shortcuts import render, get_object_or_404
-from .models import Library
+from django.shortcuts import render
+from .models import Book
 
-def library_detail(request, library_id):
-    """View to display details of a specific library."""
-    library = get_object_or_404(Library, id=library_id)
-    return render(request, 'relationship_app/library_detail.html', {'library': library})
+def list_books(request):
+    """View to list all books."""
+    books = Book.objects.all()  # Fetch all books from the database
+    return render(request, 'relationship_app/list_books.html', {'books': books})
